98042e0a544bb046e44cddfa856327c61d4a48d5
daudi44 <75979331+daudi44@users.noreply.github.com>
2021-02-10 20:33:19 +0100

    Update Pr1.java
    En aquest update esborro els comentaris que havia afegit per entendre millor les diferents parts del programa.

--- Pr1.java
+++ Pr1.java
@@ -7,14 +7,12 @@ public class Pr1 {
     public static void main(String[] args) {
         Scanner ent = new Scanner(System.in);
 
-        //Declarem les variables
         String s1, s2;
         char c1, c2;
 
         do {
             System.out.println("Introduix 2 caràcters diferents qualsevol:");
             
-            //Introcucció del primer caràcter
                 s1 = ent.nextLine();
                 if (!s1.isEmpty()) {
                     c1 = s1.charAt(0);
@@ -23,7 +21,6 @@ public class Pr1 {
                     c1 = s1.charAt(0);
                 }
 
-            //Introducció del següent caràcter
                 s2 = ent.nextLine();
                 if (!s2.isEmpty()) {
                     c2 = s2.charAt(0);
@@ -32,7 +29,6 @@ public class Pr1 {
                     c2 = s2.charAt(0);
                 }
 
-            //Mirem si els dos caràcters introduits per l'usuari són iguals o no
             if (c1 != c2) {
                 break;
             } else {
